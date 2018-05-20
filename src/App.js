dde92931e6a5ace9958c1d93a772a52bbbfbc7a8
Sua Park <parksuuuua@gmail.com>
2018-05-21 01:08:45 +0900

    #4-3 강의 예제 따라하기 내용 : data update가 생기면, 해당 데이터로 화면 업데이트 하기

--- src/App.js
+++ src/App.js
@@ -3,65 +3,57 @@ import './App.css';
 import Movie from './Movie';
 
 
-const moviesTitles = [
-  "내 아내의 모든것",
-  "About time",
-]
-
-const movieImages = [
-  "https://akns-images.eonline.com/eol_images/Entire_Site/2017524/rs_1024x565-170624152702-1024-toy-story-062417.jpg?fit=inside|900:auto&output-quality=90",
-  "https://pixar-planet.fr/wp-content/uploads/2016/10/affiche-toy-story-2-05.jpg"
-]
-
-const movies = [
-  {
-    id: 1,
-    title: "내 아내의 모든것",
-    poster: "https://akns-images.eonline.com/eol_images/Entire_Site/2017524/rs_1024x565-170624152702-1024-toy-story-062417.jpg?fit=inside|900:auto&output-quality=90"
-  },
-  {
-    id: 2,
-    title: "About time",
-    poster: "https://pixar-planet.fr/wp-content/uploads/2016/10/affiche-toy-story-2-05.jpg"
-  }
-]
-
-
 //render : componentWillMount() -> render() -> componentDidMount()
 //update : componentReceiveProps() -> shouldComponentUpdate() -> componentWillMount() -> render() -> componentDidMount()
 
 
 class App extends Component {
 
-UNSAFE_componentWillMount(){
-  console.log('unsafe will mount')
-}
-componentWillMount(){
-  console.log('will mount')
-}
 
   state = {
-    greeting: 'hello!'
+   
   }
 
   componentDidMount(){
     console.log('did mount')
     setTimeout(() => {
       this.setState({
-        greeting: 'hello again!'
-      })
-    }, 5000);
+        movies: [
+          {
+            id: 1,
+            title: "내 아내의 모든것",
+            poster: "https://akns-images.eonline.com/eol_images/Entire_Site/2017524/rs_1024x565-170624152702-1024-toy-story-062417.jpg?fit=inside|900:auto&output-quality=90"
+          },
+          {
+            id: 2,
+            title: "About time",
+            poster: "https://pixar-planet.fr/wp-content/uploads/2016/10/affiche-toy-story-2-05.jpg"
+          },
+          {
+            id: 3,
+            title: "toystory",
+            poster: "https://vignette.wikia.nocookie.net/disney/images/8/80/Toy-story2-disneyscreencaps.com-3068.jpg/revision/latest?cb=20140924011418"
+          }
+        ]
+      }) 
+
+    }, 3000);
+  }
+
+  _renderMovies = () => {
+    console.log('_renderMovies')
+    const movies = this.state.movies.map((movie) => {
+      return <Movie title={movie.title} poster={movie.poster} key={movie.id} />
+    })
+    return movies
   }
 
   render() {
     console.log('render')
     return (
-      // 수정A 참고
       <div className="App">
-        {this.state.greeting}
-        {movies.map((movie) => {
-          return <Movie title={movie.title} poster={movie.poster} key={movie.id} />
-        })}
+        {/* 데이터가 있나 없나 확인하고, 있으면 그 값을 그리라는 함수 호출, 없으면 Loading 표시 */}
+        {this.state.movies ? this._renderMovies() : 'Loading..'}
       </div>
     );
   }
@@ -69,13 +61,5 @@ componentWillMount(){
 
 export default App;
 
-
-
-// 수정A : key라는 index는 각 dataset에서 고유로 매겨야 하는 값이므로 아래와 같이 작성하지 않아야 한다. 
-{/* <div className="App">
-        {movies.map((movie,index) => {
-          return <Movie title={movie.title} poster={movie.poster} key={index} />
-        })}
-      </div> */}
 
 
